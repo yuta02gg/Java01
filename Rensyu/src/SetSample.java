3bafc950217d17fd897da10ff0dac5983e01bab9
yuta02gg <yuta02gg@outlook.jp>
2024-03-16 20:08:54 +0900

    20:08

--- Rensyu/src/SetSample.java
+++ Rensyu/src/SetSample.java
@@ -1,23 +1,22 @@
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class SetSample {
 	
-	public static void main(String[] args) {
-		Set<String>set=new TreeSet<String>();
+	public static void main(String[ ] args) {
+	Set<String> set = new HashSet<String>();
+	
+		set.add("value1");
+		set.add("value2");
+		set.add("value3");
+		set.add("value4");
+		set.add("value5");
+	
+		set.add("value2");
 		
-			set.add("value1");
-			set.add("value2");
-			set.add("value3");
-			set.add("value4");
-			set.add("value5");
-			
-			set.add("value2");
-		
-		for(String s:set) {
-			System.out.println(s);
-		}
-		
+	
+	for (String s : set){
+	  System.out.println(s);
 	}
-
+  }
 }
